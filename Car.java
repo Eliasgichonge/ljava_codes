2a6a6804c4284c76a428803fc08ef3324d2e6694
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2024-11-29 21:54:44 +0300

    instanceof Operator

---
+++ Car.java
@@ -0,0 +1,7 @@
+public class Car extends Vehicle{
+    public static void main(String[] args) {
+        Vehicle a = new Car();
+        boolean result = a instanceof Car;
+        System.out.println(result);
+    }
+}
