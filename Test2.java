04dfa272636d8992427e3df20ad217012755f936
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2024-11-29 21:51:01 +0300

    instanceof Operator

---
+++ Test2.java
@@ -0,0 +1,9 @@
+public class Test2 {
+    public static void main(String[] args) {
+        String name = "James";
+
+        // Following will return true since name is type of string
+        boolean result = name instanceof String;
+        System.out.println(result);
+    }
+}
