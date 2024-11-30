e56b0a0801180f66ad7eb79206000c4987b36a57
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2024-11-30 16:02:27 +0300

    Creating an Object

--- Puppy.java
+++ Puppy.java
@@ -2,5 +2,10 @@ public class Puppy {
     String name;
     public Puppy(String name){
         //    This constructor has onr parameter, name.
+        System.out.println("Passed Name is :" +name);
+    }
+    public static void main(String[] args) {
+        // Following statement would create an object myPuppy
+        Puppy myPuppy = new Puppy("tommy");
     }
 }
